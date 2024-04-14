307a2737208775f7e9c84228ada43add794c5950
wellitonaraujo <wellitonaraujodev@hotmail.com>
2024-04-14 17:47:08 -0300

    fix port

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
